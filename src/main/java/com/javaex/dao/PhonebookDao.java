7ec1cf0b3159e7275de84ebfb2b8d92437fbb928
yss9358 <93y2s@naver.com>
2024-02-28 17:22:09 +0900

    20240228 mybatis 오후

--- src/main/java/com/javaex/dao/PhonebookDao.java
+++ src/main/java/com/javaex/dao/PhonebookDao.java
@@ -4,11 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -23,6 +23,8 @@ public class PhonebookDao {
 
 	@Autowired
 	private DataSource dataSource;
+	@Autowired
+	private SqlSession sqlSession;
 	// 생성자
 	// 메소드-gs
 	// 메소드-일반
@@ -58,195 +60,204 @@ public class PhonebookDao {
 	// 전체가져오기
 	public List<PersonVo> personSelect() {
 
-		this.getConnection();
-
-		List<PersonVo> personList = new ArrayList<PersonVo>();
-
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " select person_id, ";
-			query += "	      name, ";
-			query += "        hp, ";
-			query += "	      company ";
-			query += " from person ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-
-			// 실행
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			while (rs.next()) {// 반복
-				int personId = rs.getInt("person_id");
-				String name = rs.getString("name");
-				String hp = rs.getString("hp");
-				String company = rs.getString("company");
-
-				// db에서 가져온 데이터 vo로 묶기
-				PersonVo personVo = new PersonVo(personId, name, hp, company);
-				// 리스트에 주소 추가
-				personList.add(personVo);
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
+		System.out.println("personSelect()");
+		List<PersonVo> personList = sqlSession.selectList("phonebook.select");
+		System.out.println(personList);
+//		this.getConnection();
+//
+//		List<PersonVo> personList = new ArrayList<PersonVo>();
+//
+//		try {
+//			// 3. SQL문 준비 / 바인딩 / 실행
+//			// SQL문 준비
+//			String query = "";
+//			query += " select person_id, ";
+//			query += "	      name, ";
+//			query += "        hp, ";
+//			query += "	      company ";
+//			query += " from person ";
+//
+//			// 바인딩
+//			pstmt = conn.prepareStatement(query);
+//
+//			// 실행
+//			rs = pstmt.executeQuery();
+//
+//			// 4.결과처리
+//			while (rs.next()) {// 반복
+//				int personId = rs.getInt("person_id");
+//				String name = rs.getString("name");
+//				String hp = rs.getString("hp");
+//				String company = rs.getString("company");
+//
+//				// db에서 가져온 데이터 vo로 묶기
+//				PersonVo personVo = new PersonVo(personId, name, hp, company);
+//				// 리스트에 주소 추가
+//				personList.add(personVo);
+//			}
+//
+//		} catch (SQLException e) {
+//			System.out.println("error:" + e);
+//		}
+//
+//		this.close();
 
 		return personList;
 	}
-
+	
 	// 등록
 	public int personInsert(PersonVo personVo) {
-		int count = -1;
-
-		this.getConnection();
+		int count = sqlSession.insert("phonebook.insert", personVo);
+		System.out.println(count);
+//		this.getConnection();
+//		
+//		try {
+//			// 3. SQL문 준비 / 바인딩 / 실행
+//			// SQL문 준비
+//			String query = "";
+//			query += " insert into person ";
+//			query += " values(null, ?, ?, ?) ";
+//
+//			// 바인딩
+//			pstmt = conn.prepareStatement(query);
+//			pstmt.setString(1, personVo.getName());
+//			pstmt.setString(2, personVo.getHp());
+//			pstmt.setString(3, personVo.getCompany());
+//
+//			// 실행
+//			count = pstmt.executeUpdate();
+//
+//			// 4.결과처리
+//			/*System.out.println(count + "건 등록되었습니다.");*/
+//
+//		} catch (SQLException e) {
+//			System.out.println("error:" + e);
+//		} 
+//		
+//		this.close();
 		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " insert into person ";
-			query += " values(null, ?, ?, ?) ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, personVo.getName());
-			pstmt.setString(2, personVo.getHp());
-			pstmt.setString(3, personVo.getCompany());
-
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			/*System.out.println(count + "건 등록되었습니다.");*/
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
-
 		return count;
 	}
 
 	// 삭제
 	public int personDelete(int no) {
-		int count = -1;
-
-		this.getConnection();
-
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " delete from person ";
-			query += " where person_id = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, no);
-
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			System.out.println(count + "건 삭제되었습니다.");
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
+		System.out.println("personDelete");
+		int count = sqlSession.delete("phonebook.delete",no);
+		System.out.println(count);
+//		this.getConnection();
+//
+//		try {
+//			// 3. SQL문 준비 / 바인딩 / 실행
+//			// SQL문 준비
+//			String query = "";
+//			query += " delete from person ";
+//			query += " where person_id = ? ";
+//
+//			// 바인딩
+//			pstmt = conn.prepareStatement(query);
+//			pstmt.setInt(1, no);
+//
+//			// 실행
+//			count = pstmt.executeUpdate();
+//
+//			// 4.결과처리
+//			System.out.println(count + "건 삭제되었습니다.");
+//
+//		} catch (SQLException e) {
+//			System.out.println("error:" + e);
+//		}
+//
+//		this.close();
 
 		return count;
 	}
 
 	// 1개 가져오기
 	public PersonVo personSelectOne(int no) {
-
-		this.getConnection();
-
-		PersonVo personVo = null;
-
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " select person_id, ";
-			query += "	      name, ";
-			query += "        hp, ";
-			query += "	      company ";
-			query += " from person ";
-			query += " where person_id=? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, no);
-
-			// 실행
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			while (rs.next()) {// 반복
-				int personId = rs.getInt("person_id");
-				String name = rs.getString("name");
-				String hp = rs.getString("hp");
-				String company = rs.getString("company");
-
-				// db에서 가져온 데이터 vo로 묶기
-				personVo = new PersonVo(personId, name, hp, company);
-
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
+		System.out.println("personSelectOne");
+		PersonVo personVo = sqlSession.selectOne("phonebook.selectOne",no);
+		System.out.println(personVo);
+		
+//		this.getConnection();
+//
+//		PersonVo personVo = null;
+//
+//		try {
+//			// 3. SQL문 준비 / 바인딩 / 실행
+//			// SQL문 준비
+//			String query = "";
+//			query += " select person_id, ";
+//			query += "	      name, ";
+//			query += "        hp, ";
+//			query += "	      company ";
+//			query += " from person ";
+//			query += " where person_id=? ";
+//
+//			// 바인딩
+//			pstmt = conn.prepareStatement(query);
+//			pstmt.setInt(1, no);
+//
+//			// 실행
+//			rs = pstmt.executeQuery();
+//
+//			// 4.결과처리
+//			while (rs.next()) {// 반복
+//				int personId = rs.getInt("person_id");
+//				String name = rs.getString("name");
+//				String hp = rs.getString("hp");
+//				String company = rs.getString("company");
+//
+//				// db에서 가져온 데이터 vo로 묶기
+//				personVo = new PersonVo(personId, name, hp, company);
+//
+//			}
+//
+//		} catch (SQLException e) {
+//			System.out.println("error:" + e);
+//		}
+//
+//		this.close();
 
 		return personVo;
 	}
 	
 	// 수정
 	public int personUpdate(PersonVo personVo) {
-		int count = -1;
-
-		this.getConnection();
-		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " update person ";
-			query += " set name=?, ";
-			query += " 	   hp=?, ";
-			query += "     company=? ";
-			query += " where person_id = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, personVo.getName());
-			pstmt.setString(2, personVo.getHp());
-			pstmt.setString(3, personVo.getCompany());
-			pstmt.setInt(4, personVo.getPersonId());
-			
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			/*System.out.println(count + "건 수정되었습니다.");*/
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
+		System.out.println("personUpdate");
+		int count = sqlSession.update("phonebook.update", personVo);
+		System.out.println(count);
+//		this.getConnection();
+//		
+//		try {
+//			// 3. SQL문 준비 / 바인딩 / 실행
+//			// SQL문 준비
+//			String query = "";
+//			query += " update person ";
+//			query += " set name=?, ";
+//			query += " 	   hp=?, ";
+//			query += "     company=? ";
+//			query += " where person_id = ? ";
+//
+//			// 바인딩
+//			pstmt = conn.prepareStatement(query);
+//			pstmt.setString(1, personVo.getName());
+//			pstmt.setString(2, personVo.getHp());
+//			pstmt.setString(3, personVo.getCompany());
+//			pstmt.setInt(4, personVo.getPersonId());
+//			
+//			// 실행
+//			count = pstmt.executeUpdate();
+//
+//			// 4.결과처리
+//			/*System.out.println(count + "건 수정되었습니다.");*/
+//
+//		} catch (SQLException e) {
+//			System.out.println("error:" + e);
+//		} 
+//		
+//		this.close();
 
 		return count;
 	}
+
 }
